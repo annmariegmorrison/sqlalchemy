192287c74a4701fa71a9352c59b7b83337c5f674
AnnMarie Morrison <annmariemorrison@AnnMaries-MacBook-Pro.local>
2020-07-02 13:55:17 -0500

    update

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-app.py
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+base = automap_base()
+base.prepare(engine, reflect=True)
+
+measurement = base.classes.measurement
+station = base.classes.station
+
+session = Session(engine) 
